8c87f996a008df1a277740287127160691fc20a5
arjxn2001 <kbarjun12@gmail.com>
2025-05-20 01:37:37 +0530

    last updations

--- src/utils/apiRequest.js
+++ src/utils/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://socialmedia-backend-fu0q.onrender.com",  //"http://localhost:3000/"
   withCredentials: true, // keep this if your backend uses cookies (optional)
 });
 
